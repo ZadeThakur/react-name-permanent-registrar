ecaa60c5e328b80520d392b4c97936e8a001d8fd
Eshu Thakur (Ujjwal Jadoun) <37092836+ZadeThakur@users.noreply.github.com>
2019-05-21 10:00:15 -0230

    Updates

---
+++ precache-manifest.bb6de2dcf201458a7d0c4433e13799dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "939d75e8221d50c36076b29334241d78",
+    "url": "/react-name-permanent-registrar/index.html"
+  },
+  {
+    "revision": "64f6023a5e0b66b0e49a",
+    "url": "/react-name-permanent-registrar/static/css/main.64c0d25a.chunk.css"
+  },
+  {
+    "revision": "8c6b377e2e491df2aa75",
+    "url": "/react-name-permanent-registrar/static/js/2.a4f98e11.chunk.js"
+  },
+  {
+    "revision": "64f6023a5e0b66b0e49a",
+    "url": "/react-name-permanent-registrar/static/js/main.99affd5d.chunk.js"
+  },
+  {
+    "revision": "57ffe3525b650ff0a9a2",
+    "url": "/react-name-permanent-registrar/static/js/runtime~main.623ced5c.js"
+  },
+  {
+    "revision": "c5d884e28fa9a2855f93a2a0e81f0f2e",
+    "url": "/react-name-permanent-registrar/static/media/image1.c5d884e2.jpg"
+  },
+  {
+    "revision": "c76c3f26ef8559058e8a48c00d781823",
+    "url": "/react-name-permanent-registrar/static/media/image2.c76c3f26.jpg"
+  }
+]);
